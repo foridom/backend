5f3164af84de6c734cef645f8a59d7b883b42883
fanzhuo <henbuhai@hotmail.com>
2020-11-03 10:00:21 +0800

    fix

--- config/backend.php
+++ config/backend.php
@@ -39,5 +39,11 @@ return [
         ],
     ],
 
+    'sms_login' => env('BACKEND_SMS_LOGIN', false),
+
+    'technical_support' => '果酱社区：https://guojiang.club',
+
+    'copyright' => '果酱社区',
+
     'scenario' => env('BACKEND_SCENARIO', 'normal')
 ];
